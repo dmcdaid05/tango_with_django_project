ce26c7779930040407ed85d7f0882c08336f9556
Daniel McDaid <2870251m@student.gla.ac.uk>
2025-02-13 21:34:45 +0000

    Chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,8 @@ app_name = 'rango'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+       views.show_category, name='show_category'),
+    path('page/<slug:page_name_slug>/',
+       views.show_page, name='show_page')
 ]
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/images/rango.jpg/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
